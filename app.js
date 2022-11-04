c25433fcc3d98df9423694c3b8c5c5f213e26793
Tiago Cruz <sathoskath@gmail.com>
2022-11-04 15:34:24 -0300

    added google authentication

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const {engine} = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
-dotenv.config({path: "./config/config.env"})
+dotenv.config({path: "./config/config.env"});
+
+// Passport config
+require('./config/passport') (passport);
 
 // Connect to mongoDB
 connectDB();
@@ -23,11 +28,22 @@ const PORT = process.env.PORT;
 app.engine('.hbs', engine({defaultLayout: 'mainLayout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret: 'chrono trigger',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static folder
 app.use(express.static('public'))
 
 // Routes
 app.use('/', require('./routes/index'))
-
+app.use('/auth', require('./routes/auth'))
 
 app.listen(PORT, console.log(`Server connected in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`))
